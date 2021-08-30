5b871e9b84a0ade96eb10405df8d840a780568d4
haneenbader <haneenalwatan993@gmail.com>
2021-08-30 19:04:52 +0300

    solve part of lab 27

--- TaskMaster/app/src/main/java/com/example/taskmaster/MainActivity.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // target to button add task
+        Button addTask = findViewById(R.id.savesetting);
+        //add eventListener
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddTasks = new Intent(MainActivity.this, AddTask.class);
+                startActivity(goToAddTasks);
+            }
+        });
+
+
+
+        // target to button all task
+        Button allTask = findViewById(R.id.alltask);
+        //add eventListener
+        allTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent goToAllTasks = new Intent(MainActivity.this, AllTask.class);
+                startActivity(goToAllTasks);
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "Override onStart()", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Override onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "Override onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "Override onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "Override onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "Override onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
 }
--- TaskMaster/app/src/main/java/com/example/taskmaster/SettingsPage.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/SettingsPage.java
@@ -7,23 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class AllTask extends AppCompatActivity {
+public class SettingsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_task);
+        setContentView(R.layout.activity_settings_page);
 
-
-        // target to button home page
-        Button homePage = findViewById(R.id.homepage);
+// target to button home page
+        Button homePage = findViewById(R.id.tohomepage);
         //add eventListener
         homePage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHomePage = new Intent(AllTask.this , MainActivity.class);
+                Intent goToHomePage = new Intent(SettingsPage.this , MainActivity.class);
                 startActivity(goToHomePage);
             }
         });
+
     }
 }
