a81df778dc46ae517a324b1f035a18ada953dfc3
haneenbader <haneenalwatan993@gmail.com>
2021-08-30 23:26:09 +0300

    finish lab27

--- TaskMaster/app/src/main/java/com/example/taskmaster/MainActivity.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -87,39 +92,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(), "Override onStart()", Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Toast.makeText(getApplicationContext(), "Override onStart()", Toast.LENGTH_SHORT).show();
+//    }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "Override onResume()", Toast.LENGTH_SHORT).show();
+        String userTaskMessage = "’s tasks";
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String userName = sharedPreferences.getString("userName","user");
+
+        TextView textViewUserName = findViewById(R.id.textViewusername);
+        textViewUserName.setText(userName +userTaskMessage );
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), "Override onPause()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "Override onStop()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getApplicationContext(), "Override onRestart()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), "Override onDestroy()", Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Toast.makeText(getApplicationContext(), "Override onPause()", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Toast.makeText(getApplicationContext(), "Override onStop()", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Toast.makeText(getApplicationContext(), "Override onRestart()", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(getApplicationContext(), "Override onDestroy()", Toast.LENGTH_SHORT).show();
+//    }
 
 }
--- TaskMaster/app/src/main/java/com/example/taskmaster/SettingsPage.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/SettingsPage.java
@@ -3,9 +3,12 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsPage extends AppCompatActivity {
 
@@ -25,5 +28,24 @@ public class SettingsPage extends AppCompatActivity {
             }
         });
 
+
+
+        Button save = findViewById(R.id.savesetting);
+        // same as above
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SettingsPage.this);
+                SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+
+                EditText editTextUserName = findViewById(R.id.edittextusername);
+                String userName = editTextUserName.getText().toString();
+
+                sharedPreferencesEditor.putString("userName", userName);
+                sharedPreferencesEditor.apply();
+            }
+        });
     }
+
+
 }
